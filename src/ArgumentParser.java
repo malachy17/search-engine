557b565e681aa738c5b36a7778d0ad4a6e84939b
malachylin-nugent <malachylin-nugent@10.1.24.215>
2016-09-19 17:37:22 -0700

    Commit done 1

---
+++ src/ArgumentParser.java
@@ -0,0 +1,70 @@
+import java.util.HashMap;
+
+public class ArgumentParser {
+
+    private final HashMap<String, String> argumentMap;
+
+    public ArgumentParser() {
+        argumentMap = new HashMap<>();
+    }
+
+    public ArgumentParser(String[] args) {
+        this();
+        parseArguments(args);
+    }
+
+    public void parseArguments(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+        	if ((args[i].charAt(0) == '-') && (args[i].length() > 1)) {
+        		if ((args[i+1].charAt(0) != '-') && (args[i+1].length() > 1)) {
+        			if (!argumentMap.containsKey(args[i])) {
+        				argumentMap.put(args[i], args[i+1]);
+        			}
+        			else {
+        				argumentMap.remove(args[i]);
+        				argumentMap.put(args[i], args[i+1]);
+        			}
+        		}
+        		else {
+        			argumentMap.put(args[i], null);
+        		}
+        	}
+        }
+    }
+
+    public int numFlags() {
+    	return argumentMap.size();
+    }
+
+    public boolean hasFlag(String flag) {
+    	if (argumentMap.containsKey(flag)) {
+    		return true;
+    	}
+        return false;
+    }
+
+    public boolean hasValue(String flag) {
+    	if (argumentMap.get(flag) != null) {
+    		return true;
+    	}
+        return false;
+    }
+
+    public String getValue(String flag) {
+    	if (argumentMap.get(flag) != null) {
+    		return argumentMap.get(flag);
+    	}
+        return null;
+    }
+
+    public String getValue(String flag, String defaultValue) {
+    	if (argumentMap.get(flag) != null) {
+    		return argumentMap.get(flag);
+    	}
+        return defaultValue;
+    }
+
+    public String toString() {
+        return argumentMap.toString();
+    }
+}
--- src/Driver.java
+++ src/Driver.java
@@ -3,11 +3,12 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Driver {
-
+ 
     public static void main(String[] args) throws IOException {
     	
-    	Path path = Paths.get(args[0]);
-    	Path outFile = Paths.get(args[1]);
+    	ArgumentParser argsParser = new ArgumentParser(String[] args);
+    	
+    	Path path = Paths.get(argsParser.getValue("-dir"));
     	
     	// Create the inverted index data structure.
     	InvertedIndex index = new InvertedIndex();
@@ -16,9 +17,13 @@ public class Driver {
     	DirectoryStreamer streamer = new DirectoryStreamer(path, index);
     	streamer.startTraversing();
     	
-    	// Write out the inverted index data structure in JSON.
-    	JSONWriter writer = new JSONWriter(outFile, index);
-    	writer.startWriting();
+    	// Write out index onto a JSON file.
+    	if (argsParser.hasFlag("-index")) {
+    		Path outFile = Paths.get(argsParser.getValue("-index", "index.json"));
+        	JSONWriter writer = new JSONWriter(outFile, index);
+        	writer.startWriting();
+    	}
+    	
     	
     }
 }
