7282d0713d16e81b6467906c46ae5326fb40ae5a
Sophie Engle <sjengle@cs.usfca.edu>
2016-09-23 17:29:06 -0700

    Code review

--- src/Driver.java
+++ src/Driver.java
@@ -3,28 +3,33 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Driver {
- 
-    public static void main(String[] args) throws IOException {
-    	
-    	ArgumentParser argsParser = new ArgumentParser(args);
-    	
-    	try {
-    		Path path = Paths.get(argsParser.getValue("-dir"));
-    	
-    		// Create the inverted index data structure.
-    		InvertedIndex index = new InvertedIndex();
-    	
-    		// Fill the inverted index data structure.
-    		DirectoryStreamer streamer = new DirectoryStreamer(path, index);
-    		streamer.startTraversing();
-    	
-    		// Write out index onto a JSON file.
-    		Path outFile = Paths.get(argsParser.getValue("-index", "index.json"));
-    		JSONWriter writer = new JSONWriter(outFile, index);
-    		writer.startWriting();
-    	}
-    	catch (NullPointerException e) {
-    		System.err.println("You must enter a directory!");
-    	} 	
-    }
+
+	// TODO main should never throw exceptions, handle them here
+	public static void main(String[] args) throws IOException {
+
+		ArgumentParser argsParser = new ArgumentParser(args);
+
+		try {
+			// TODO Check if there is a -dir flag first, only build if there is one
+			Path path = Paths.get(argsParser.getValue("-dir"));
+
+			// Create the inverted index data structure.
+			InvertedIndex index = new InvertedIndex();
+
+			// Fill the inverted index data structure.
+			InvertedIndexBuilder streamer = new InvertedIndexBuilder(path, index);
+			streamer.startTraversing();
+			
+			// TODO InvertedIndexBuilder.startTraversing(path, index);
+
+			// TODO only write to JSON if the -index flag is present
+			// Write out index onto a JSON file. if (argsParser.hasFlag(-index))
+			Path outFile = Paths.get(argsParser.getValue("-index", "index.json"));
+			JSONWriter writer = new JSONWriter(outFile, index);
+			writer.startWriting();
+		}
+		catch (NullPointerException e) {
+			System.err.println("You must enter a directory!");
+		} 	
+	}
 }
